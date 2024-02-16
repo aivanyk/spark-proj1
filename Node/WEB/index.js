4068015b02eb65f0704cf2ea808e8824353c1dea
hzy <1543012653@qq.com>
2024-02-16 14:40:05 -0500

    fix

--- Node/WEB/index.js
+++ Node/WEB/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use('/', express.static('frontend/build'));
+// app.use('/', express.static('frontend/build'));
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
 
 app.get('/api', (req, res) => {
   res.send('Hello, world!');
