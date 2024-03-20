b539ad3c411c4aa6e6d1270734a52afe9ab58a00
hzy <1543012653@qq.com>
2024-03-20 16:36:22 -0400

    assign4-Q1.1

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -1,7 +1,28 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { DefaultAzureCredential } from "@azure/identity";
+import { SecretClient } from "@azure/keyvault-secrets";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
+
+    const keyVaultName = "assign4KV";
+    const keyVaultUrl = `https://${keyVaultName}.vault.azure.net/`;
+    const secretName = "secret2";
+
+    const credential = new DefaultAzureCredential();
+    const secretClient = new SecretClient(keyVaultUrl, credential);
+
+    let secretValue = "";
+    try {
+        // Retrieve the secret value from Azure Key Vault
+        const secretBundle = await secretClient.getSecret(secretName);
+        secretValue = secretBundle.value;
+        context.log(`The value of the secret is: ${secretValue}`);
+    } catch (error) {
+        context.log.error(`Failed to retrieve the secret from Azure Key Vault: ${error}`);
+        secretValue = "Error retrieving the secret.";
+    }
+
 
     // Get the current date
     const today = new Date();
