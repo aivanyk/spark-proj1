0fe3494830ef728d2ca57782b6809ab1932a4a1e
hzy <1543012653@qq.com>
2024-03-20 16:15:27 -0400

    assign4-Q1.1

--- Node/WEB/index.js
+++ Node/WEB/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const { DefaultAzureCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
+
 const app = express();
 const port = process.env.PORT || 3000
 
+const keyVaultName = "assign4KV";
+const keyVaultUrl = `https://${keyVaultName}.vault.azure.net/`;
+const secretName = "secret1";
+
+const credential = new DefaultAzureCredential();
+const client = new SecretClient(keyVaultUrl, credential);
+
 app.use('/', express.static('frontend/build'));
 
-app.get('/api', (req, res) => {
-  res.send('Hello, world!');
+// app.get('/api', (req, res) => {
+//   res.send('Hello, world!');
+// });
+
+app.get('/api', async (req, res) => {
+  try {
+    // Fetch the secret value from Azure Key Vault
+    const secretBundle = await client.getSecret(secretName);
+    res.send(`Secret Value: ${secretBundle.value}`);
+  } catch (error) {
+    console.error("Error retrieving secret from Azure Key Vault:", error);
+    res.status(500).send('Error retrieving secret from Azure Key Vault');
+  }
 });
 
 app.listen(port, () => {
