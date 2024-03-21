a6b222672a73fbad96b8b437a01d9d1d5c78c039
hzy <1543012653@qq.com>
2024-03-21 14:20:35 -0400

    assign4Q3

--- Node/API2/QueueProcessor/index.js
+++ Node/API2/QueueProcessor/index.js
@@ -1,5 +1,6 @@
 module.exports = async function (context, myQueueItem) {
-    context.log('JavaScript queue trigger function processed work item', myQueueItem);
+    context.log('JavaScript queue trigger function processed work item: ', myQueueItem);
+    context.log('The third secret is: ', process.env.kvsecret3);
     context.log('expirationTime =', context.bindingData.expirationTime);
     context.log('insertionTime =', context.bindingData.insertionTime);
     context.log('nextVisibleTime =', context.bindingData.nextVisibleTime);
